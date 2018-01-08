1bac1dd7134c06b169f732bda1dc1a6ae7b0a2f8
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-01-08 09:53:17 -0700

    Async warmup

--- AsyncAwait/main.js
+++ AsyncAwait/main.js
@@ -2,15 +2,22 @@ var FortuneTeller = require('galvanize-game-mechanics').FortuneTeller
 
 // FortuneTeller has an "ask" method that returns a promise. If you wanted to write a function that used the FortuneTeller.ask method to ask to questions (waiting for the first one to finish before asking the second) with promises, you would need to use .then like this:
 
-const askQuestionPromise = (question) => {
-  FortuneTeller.ask(question).then(result => {
-    console.log('Promise 1:', result.response)
-    FortuneTeller.ask(question).then(result => {
-      console.log('Promise 2:', result.response)
-    })
-  })
+// const askQuestionPromise = (question) => {
+//   FortuneTeller.ask(question).then(result => {
+//     console.log('Promise 1:', result.response)
+//     FortuneTeller.ask(question).then(result => {
+//       console.log('Promise 2:', result.response)
+//     })
+//   })
+// }
+
+// askQuestionPromise("Why is the sky blue?")
+
+const askQuestionPromise = async (question) => {
+  let fortune1 = await FortuneTeller.ask(question)
+  // let fortune2 = FortuneTeller.ask(question) // NOT CORRECT
+  console.log('Promise 1:', fortune1.response)
+  // console.log('Promise 2:', response await fortune2) // NOT CORRECT
 }
 
 askQuestionPromise("Why is the sky blue?")
-
-// How could you rewrite the askQuestion function using async/await?
